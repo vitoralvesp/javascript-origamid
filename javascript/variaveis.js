da030028e2a0d0bc6c6fa2a1f24e07ed78e7052c
Vitor Alves Pereira <68354632+vitoralvesp@users.noreply.github.com>
2021-02-25 15:14:21 -0300

    🛸 loading  🛸

---
+++ javascript/variaveis.js
@@ -0,0 +1,28 @@
+var nome = "Vitor";
+console.log(nome);
+
+var sobrenome = "Pereira", cidade = "São Paulo", profissão = "desenvolvedor";
+
+var idade = 12;
+idade = 32;
+
+
+
+// Declarar uma variável com o seu nome 
+const name = "Vitor";
+
+// Declarar uma variável com a sua idade 
+let age = 17;
+
+// Declarar uma variável com a sua comida favorita e não atribuir valor
+let notExists;
+
+// Atribuir valor à sua comida favorita 
+notExists = null;
+
+// Declarar 5 variáveis diferentes sem valores 
+var whatIsYourName;
+var whatIsYourAge;
+var whatIsYourAddress;
+var whatIsYourParents;
+var whatIsYourFavoriteMovie;
