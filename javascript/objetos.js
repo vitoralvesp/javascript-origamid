3798f6611e66bfa10dbddb047c3fda99f512651b
Vitor Alves Pereira <68354632+vitoralvesp@users.noreply.github.com>
2021-02-28 18:47:22 -0300

    :fire:

---
+++ javascript/objetos.js
@@ -0,0 +1,27 @@
+// Método
+var quadrado = {
+    lados: 4,
+    area = function(lado) {
+        return lado * lado;
+    },
+    perimetro = function(lados) {
+        this.lados * lado;
+    },
+}
+
+
+// Crie um objeto com seus dados pessoais
+var pessoa = {
+    nome: "Vitor",
+    sobrenome: "LINDO",
+    idade: 17,
+    profissao: "Desenvolvedor",
+    possuiFaculdade: false,
+}
+
+// crie um método no objeto anterior que mostre o seu nome completo
+pessoa.completeName = function() {
+    return `${this.nome} ${this.sobrenome}`;
+}; 
+
+
