28b3cce11c1625faf63f9db5daee2af1175d4baa
Vitor Alves Pereira <pereira.alves.vitor@outlook.com>
2021-03-01 21:31:45 -0300

    🌎 new file 🌎

---
+++ javascript/objetos-2.js
@@ -0,0 +1,14 @@
+var nome = "Vitor";
+
+var lengthName = nome.length;
+var tinyName = nome.toLowerCase();
+
+
+// nomeie propriedades ou métodos de elementos do DOM
+var btn = document.querySelector('.btn');
+ 
+
+// busque na web um objeto capaz de interagir com o clipboard (parte do seu computador que lida com o CTRL + C e CTRL + V)
+
+
+
