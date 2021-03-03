3ae8dcc55bb523eddd1f4d449f08bd358628c900
Vitor Alves Pereira <pereira.alves.vitor@outlook.com>
2021-03-03 13:48:59 -0300

    :ant: learning :ant:

---
+++ javascript/dom.js
@@ -0,0 +1,18 @@
+window.alert('Masaka!');
+
+
+// Retorne o url da página atual utilizando o objeto window
+const url = window.location.href;
+
+
+// Selecione o primeiro elemento da página que possua a classe 'ativo'
+const activeElement = document.querySelector('.active');
+
+
+// Retorne a linguagem do navegador
+const language = window.navigator.language;
+
+
+// Retorne a largura da janela
+const widthWindow = window.innerWidth;
+
