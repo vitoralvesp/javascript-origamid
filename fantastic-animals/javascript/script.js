44f24cb667e6ed1cd4fac5d862bc879679b3908c
Vitor Alves Pereira <pereira.alves.vitor@outlook.com>
2021-03-04 08:53:41 -0300

    :fire: new update :fire:

--- fantastic-animals/javascript/script.js
+++ fantastic-animals/javascript/script.js
@@ -1,1 +1,19 @@
-const animais = document.getElementById('animais');
+// Retorne no console todas as imagens do site
+const images = document.querySelectorAll('img');
+//console.log(images);
+
+// Retorne no console apenas as imagens que começaram com a palavra imagem
+const string = document.querySelectorAll('img[src^="img/imagem"]');
+//console.log(string);
+
+// Selecione os links internos, onde o href começa com #
+const internLinks = document.querySelectorAll('[href^="#"]');
+//console.log(internLinks);
+
+// Selecione o primeiro h2 dentro de .animais-descricao
+const h2Element = document.querySelectorAll('.animais-descricao h2');
+//console.log(h2Element);
+
+// Selecione o último p do site
+const paragraphElement = document.querySelectorAll('p');
+//console.log(paragraphElement[paragraphElement.length - 1]);
