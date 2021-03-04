765f6d6fe9d21954678061d030ea27d9423144b1
Vitor Alves Pereira <pereira.alves.vitor@outlook.com>
2021-03-04 17:59:28 -0300

    :fireworks: file :fireworks:

--- fantastic-animals/javascript/script.js
+++ fantastic-animals/javascript/script.js
@@ -17,3 +17,34 @@ const h2Element = document.querySelectorAll('.animais-descricao h2');
 // Selecione o último p do site
 const paragraphElement = document.querySelectorAll('p');
 //console.log(paragraphElement[paragraphElement.length - 1]);
+
+
+// Mostre o texto dos parágrafos no console
+paragraphElement.forEach((element) => console.log(element));
+
+// Mostre o texto dos parágrafos
+paragraphElement.forEach((element) => console.log(element.innerHTML));
+
+
+// Adicione a classe 'ativo' a todos os itens do menu
+const menu = document.querySelectorAll('.menu a');
+menu.forEach((menu) => {
+    menu.classList.add('ativo');
+})
+
+// Remove a classe 'ativo' a todos os itens do menu, menos o primeiro
+menu.forEach((menu) => {
+    menu.classList.remove('ativo');
+})
+
+menu[0].classList.add('ativo');
+
+// Verifique se as imagens possuem o atributo alt
+images.forEach((image) => {
+    const attribute = image.hasAttribute('alt');
+    console.log(img. attribute);
+})
+
+// Modifique o link interno
+const link = document.querySelector('a[href^="http"');
+link.setAttribute('href',  'www.google.com');
