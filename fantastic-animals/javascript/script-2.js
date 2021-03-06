3b086bce1b2feb20bce451e0220ed954042db363
Vitor Alves Pereira <pereira.alves.vitor@outlook.com>
2021-03-06 14:52:53 -0300

    :alien: file :alien:

--- fantastic-animals/javascript/script-2.js
+++ fantastic-animals/javascript/script-2.js
@@ -27,3 +27,26 @@ function handleLink(event) {
 internLinks.forEach((link) => {
     link.addEventListener('click', handleLink);
 });
+
+
+// Duplique o menu e adicione ele em copyright
+const menu = document.querySelector('.menu');
+const copyright = document.querySelector('.copy')
+const clone = menu.cloneNode(true);
+copyright.appendChild(clone);
+
+
+
+// Selecione o primeiro DT da dl de Faq
+const faq = document.querySelector('.faq');
+const dt = faq.querySelector('dt');  
+
+// Selecione o DD referente ao primeiro dt 
+const nextDt = dt.nextElementSibling;
+
+// Substitua o conteúdo de Faq pelo de animais
+const animals = document.querySelector('.animais');
+faq.innerHTML = animals.innerHTML;
+
+
+
