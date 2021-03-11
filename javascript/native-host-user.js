076002628e51594ccbe914f91d44348d75910e46
Vitor Alves Pereira <pereira.alves.vitor@outlook.com>
2021-03-11 16:36:27 -0300

    :alien:

---
+++ javascript/native-host-user.js
@@ -0,0 +1,19 @@
+// Liste 5 objetos nativos
+Object
+String
+Array
+Function
+Number
+
+
+// Liste 5 objetos do browser
+Window
+history
+document
+HTMLCollection
+NodeList
+
+
+// Liste 2 Métodos, Propriedades ou Objetos que não existem no Firefox, somente no Chrome
+ webkitHidden
+ webkitCancelAnimationFrame
