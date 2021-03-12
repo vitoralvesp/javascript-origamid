b4c3cff875dfc97d26d2dc2185c6e763533477ef
Vitor Alves Pereira <pereira.alves.vitor@outlook.com>
2021-03-12 19:21:51 -0300

    :ant: new file :ant:

---
+++ javascript/objetos-3.js
@@ -0,0 +1,9 @@
+const comida = 'Pizza'; // return a string
+const agua = new String('Água'); // return a object
+
+const frase = 'A melhor comida';
+frase[frase.length - 1]; // return a
+
+
+frase.charAt(0); // return A 
+frase.charAt(frase.length - 1); // return a
