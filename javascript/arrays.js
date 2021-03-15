5848e9ee1b291f3120fb4a4dd66480be6380a1de
Vitor Alves Pereira <pereira.alves.vitor@outlook.com>
2021-03-15 15:29:50 -0300

    :ant: learning :ant:

--- javascript/arrays.js
+++ javascript/arrays.js
@@ -22,10 +22,26 @@ console.log(comidas); // return ['Arroz', 'Frango', 'Carne', 'Peixe', 'Batata']
 
 
 const estudantes = ['Marcio', 'Brenda', 'Joana', 'Kleber', 'Julia'];
+
 // Arrume os estudantes em ordem alfabética
+const organizedStudents = estudantes.sort();
+console.log(organizedStudents);
+
+
 // Inverta a ordem dos estudantes
+const disorganizedStudents = estudantes.reverse();
+console.log(disorganizedStudents);
+
+
 // Verifique se Joana faz parte dos estudantes
+const joanaIncludes = estudantes.includes('Joana');
+console.log(joanaIncludes); // return true
+
+
 // Verifique se Juliana faz parte dos estudantes
+const julianaIncludes = estudantes.includes('Juliana');
+console.log(julianaIncludes); // return false
+
 
 let html = `<section>
               <div>Sobre</div>
@@ -34,7 +50,19 @@ let html = `<section>
             </section>`
 // Substitua section por ul e div com li,
 // utilizando split e join
+const removeSection = html.split('section');
+const removeDiv = html.split('div');
+
+// const addUlElement = html.join('ul');
+// const addLiElement = html.join('li');
+
 
 const carros = ['Ford', 'Fiat', 'VW', 'Honda'];
+
 // Remova o último carro, mas antes de remover
 // salve a array original em outra variável
+const copy = carros.copyWithin(0); 
+console.log(copy); // return ['Ford', 'Fiat', 'VW', 'Honda']
+
+const removeElement = carros.pop();
+console.log(removeElement); // return 'Honda'
