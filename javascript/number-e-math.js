f84227c99a6deb2ee5e1cd25bd138348fda2e46b
Vitor Alves Pereira <pereira.alves.vitor@outlook.com>
2021-03-15 09:32:31 -0300

    :ant: learning :ant:

--- javascript/number-e-math.js
+++ javascript/number-e-math.js
@@ -14,3 +14,46 @@ preço.toLocaleString('en-US', {style: 'currency', currency: 'USD'}); // return 
 preço.toLocaleString('pt-BR', {style: 'currency', currency: 'BRL'}); // return R$2.99
 
 Math.random() * 100 // retorna números de 0 até 100
+
+
+
+// Retorne um número aleatório de 1050 a 2000
+const number = Math.floor(Math.random() * (2000 - 1050 + 1) + 1050);
+
+
+// Retorne o maior número da lista abaixo
+const listNumbers = '4, 5, 20, 8, 9';
+const arrayNumbers = listNumbers.split(', ');
+const maxNumber = Math.max(...arrayNumbers); // return 20
+
+
+// Crie uma função para limpar os preços e retornar a soma total
+const listaPrecos = ['R$ 59,99', 'R$ 100,222', 'R$ 230  ', 'r$ 200'];
+
+function limparPreco(preco) {
+    preco = +preco.toUpperCase().replace(',', '.').trim('R$', '');
+    preco = preco.toFixed(2);
+    return preco;
+};
+
+let soma = 0;
+listaPrecos.forEach((preco) => {
+    soma = soma + limparPreco(preco);
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
