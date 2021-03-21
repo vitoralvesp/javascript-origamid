9ac72af63ce556da3931854be54e3a1570196eee
Vitor Alves Pereira <pereira.alves.vitor@outlook.com>
2021-03-21 19:05:06 -0300

    :whale2:

---
+++ javascript/object.js
@@ -0,0 +1,10 @@
+const carro = {
+    carro: carro,
+    marca: marca
+}
+
+const amigos = new Object({
+    Thomas_Shelby: shelby_family,
+    Christopher_Nolan: chris_nolan,
+    Ragnar_Lothbrock: the_best_viking
+});
