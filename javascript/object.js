16cb63808fa39381a002256dcb041d60078cd161
Vitor Alves Pereira <pereira.alves.vitor@outlook.com>
2021-03-21 19:36:46 -0300

    :whale2:

--- javascript/object.js
+++ javascript/object.js
@@ -1,10 +1,33 @@
 const carro = {
-    carro: carro,
-    marca: marca
+    carro: 'Carro',
+    marca: 'Marca',
 }
 
-const amigos = new Object({
-    Thomas_Shelby: shelby_family,
-    Christopher_Nolan: chris_nolan,
-    Ragnar_Lothbrock: the_best_viking
+const honda = Object.create(carro);
+honda.marca = 'Honda';
+honda.carro = 'HRV';
+
+const friends = new Object({
+    Thomas_Shelby: 'shelby_family',
+    Christopher_Nolan: 'chris_nolan',
+    Ragnar_Lothbrock: 'the_best_viking',
 });
+
+const viking = {};
+
+Object.defineProperties(viking, {
+    sword: {
+        value: true,
+        configurable: false
+    },
+    axe: {
+        value: true,
+        configurable: false
+    },
+    shield: {
+        value: true,
+        configurable: false
+    }
+});
+
+console.log(viking)
