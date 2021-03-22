63e9a42ae9e1a69bcbb6f950a7b7c66d7ca917c0
Vitor Alves Pereira <pereira.alves.vitor@outlook.com>
2021-03-22 19:59:13 -0300

    :ant: learning :ant:

--- javascript/object.js
+++ javascript/object.js
@@ -30,4 +30,29 @@ Object.defineProperties(viking, {
     }
 });
 
-console.log(viking)
+console.log(viking);
+
+
+console.log(Object.keys(carro)); // return {"carro", "marca"} 
+console.log(Object.entries(carro)); // return {["carro", "Carro"], ["marca", "Marca]"}
+
+
+const fruta = ["Banana"];
+console.log(Object.getPrototypeOf(fruta)); // [constructor: ƒ, concat: ƒ, copyWithin: ƒ, fill: ƒ, find: ƒ, …]
+
+const fruta2 = ["Banana"];
+console.log(Object.is(fruta, fruta2)); // return false
+
+
+
+Object.freeze(carro); // impede que o valor das propriedades sejam alteradas
+Object.preventExtensions(carro); // previne a adição de novas propriedades 
+Object.seal(carro); // permite que as propriedades sejm alteradas, porém não permite que alguma delas seja deletada 
+
+
+console.log(Object.isFrozen(carro)); // true
+console.log(Object.isExtensible(carro)); // true
+console.log(Object.isSealed(carro)); // true
+
+
+
