12d31527dff1b5fcf4911355adabb2c662183a84
Vitor Alves Pereira <pereira.alves.vitor@outlook.com>
2021-03-22 20:09:34 -0300

    :ant: learning :ant:

--- javascript/object.js
+++ javascript/object.js
@@ -55,4 +55,28 @@ console.log(Object.isExtensible(carro)); // true
 console.log(Object.isSealed(carro)); // true
 
 
+fruta.hasOwnProperty('map'); // false
+Array.prototype.isPrototypeOf(fruta); // true
+
+
+// Crie uma função que verifique corretamente o tipo de dado
+
+
+// Crie um objeto quadrado com
+// a propriedade lados e torne
+// ela imutável
+
+// Previna qualquer mudança
+// no objeto abaixo
+const configuracao = {
+    width: 800,
+    height: 600,
+    background: '#333'
+}
+  
+// Liste o nome de todas
+// as propriedades do
+// protótipo de String e Array
+  
+
 
