da5ed86deb0942d63e60e67b9d6e81efa3dbade3
Vitor Alves Pereira <pereira.alves.vitor@outlook.com>
2021-03-22 20:23:29 -0300

    :ant: learning :ant:

--- javascript/object.js
+++ javascript/object.js
@@ -60,23 +60,40 @@ Array.prototype.isPrototypeOf(fruta); // true
 
 
 // Crie uma função que verifique corretamente o tipo de dado
+function checkData(dado) {
+    return Object.prototype.toString.call(dado);
+}
+
+checkData();
+
+// Crie um objeto quadrado com a propriedade lados e torne ela imutável
+const quadrado = {
+    lados: 12,
+}
+
+Object.freeze(quadrado);
 
 
-// Crie um objeto quadrado com
-// a propriedade lados e torne
-// ela imutável
+const cubo = {};
+Object.defineProperties(cubo, {
+    faces: {
+        value: 6,
+        enumerable: true
+    }
+})
 
-// Previna qualquer mudança
-// no objeto abaixo
+// Previna qualquer mudança no objeto abaixo
 const configuracao = {
     width: 800,
     height: 600,
     background: '#333'
 }
+
+Object.freeze(configuracao);
   
-// Liste o nome de todas
-// as propriedades do
-// protótipo de String e Array
+// Liste o nome de todas as propriedades do protótipo de String e Array
+console.log(Object.getOwnPropertyNames(String.prototype));
+console.log(Object.getOwnPropertyNames(Array.prototype));
   
 
 
