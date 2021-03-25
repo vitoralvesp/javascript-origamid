aafadb7f25d82eca88d16c4ae12286e07f3b7335
Vitor Alves Pereira <pereira.alves.vitor@outlook.com>
2021-03-25 16:54:32 -0300

    :whale:

---
+++ fantastic-animals/javascript/script-animation.js
@@ -0,0 +1,9 @@
+import initScroll from './init-scroll.js';
+import initAnimacaoScroll from './init-animacao-scroll.js';
+import initAccordion from './init-accordion.js';
+import initTabNav from './tab-nav.js'
+
+initScroll();
+initAnimacaoScroll();
+initAccordion();
+initTabNav();
