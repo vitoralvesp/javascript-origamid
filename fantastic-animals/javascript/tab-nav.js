92199ac9fe6c141441457dd61b08e0dc3521e829
Vitor Alves Pereira <pereira.alves.vitor@outlook.com>
2021-03-25 16:55:39 -0300

    :whale:

---
+++ fantastic-animals/javascript/tab-nav.js
@@ -0,0 +1,23 @@
+export default function initTabNav() {
+    const tabMenu = document.querySelectorAll('.js-tabmenu li');
+    const tabContent = document.querySelectorAll('.js-tabcontent section');
+    
+    if (tabMenu.length && tabContent.length) {
+        tabContent[0].classList.add('active');
+    
+        function activeTab(index) {
+            tabContent.forEach((section) => {
+                section.classList.remove('active');
+            });
+        
+            tabContent[index].classList.add('active');
+        }
+        
+        tabMenu.forEach((itemMenu, index) => {
+            itemMenu.addEventListener('click', function() {
+                activeTab(index);
+            });
+        });
+    }
+};
+
