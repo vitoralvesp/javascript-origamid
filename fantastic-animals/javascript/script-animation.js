24c8cf8636f6a0e4821271ce9e99ffb7e66a3dd8
Vitor Alves Pereira <pereira.alves.vitor@outlook.com>
2021-03-28 17:51:04 -0300

    :alien:

--- fantastic-animals/javascript/script-animation.js
+++ fantastic-animals/javascript/script-animation.js
@@ -3,9 +3,11 @@ import initAnimacaoScroll from './init-animacao-scroll.js';
 import initAccordion from './init-accordion.js';
 import initTabNav from './tab-nav.js';
 import initModal from './modal.js';
+import initTooltip from './tooltip.js';
 
 initScroll();
 initAnimacaoScroll();
 initAccordion();
 initTabNav();
 initModal();
+initTooltip();
