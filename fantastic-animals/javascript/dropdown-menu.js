c3ede9a080f94c9d5fa0bc97a9f5db8e4b8043d2
Vitor Alves Pereira <pereira.alves.vitor@outlook.com>
2021-03-29 11:15:32 -0300

    :alien:

--- fantastic-animals/javascript/dropdown-menu.js
+++ fantastic-animals/javascript/dropdown-menu.js
@@ -1,3 +1,15 @@
 export default function initDropdownMenu() {
+    const dropdownMenu = document.querySelectorAll('[data-dropdown');
+
+    dropdownMenu.forEach(menu => {
+        ['touchstart', 'click'].forEach(userEvent => {
+            menu.addEventListener(userEvent, handleClick);
+        }); 
+    });
     
+    function handleClick(event) {
+        event.preventDefault();
+        this.classList.toggle('active');
+    }
 }
+
