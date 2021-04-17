f24940a6871c9d6e441371a2b602e6ff2df069ec
Vitor Alves Pereira <pereira.alves.vitor@outlook.com>
2021-04-17 12:28:16 -0300

    :octocat:

--- fetch/script-2.js
+++ fetch/script-2.js
@@ -6,3 +6,34 @@ image.then(i => i.blob())
     const imgDom = document.querySelector('img');
     imgDom.src = blobUrl;
 });
+
+// JSON
+const json = fetch('https://viacep.com.br/ws/01001000/json/');
+
+json.then(i => {
+    const i2 = i.clone();
+    i.text().then((text) => {
+        console.log(text)
+    })
+    i2.json().then((json) => {
+        console.log(json);
+    })
+})
+.then(body => {
+    console.log(body);
+})
+
+// Headers
+image.then(response => {
+    console.log(response);
+    response.headers.forEach(console.log);
+});
+
+// Status
+image.then(response => {
+    console.log(response.status);
+    if(response.status === 404) {
+        return 'Página inexistente'
+    }
+})
+
